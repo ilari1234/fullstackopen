64c8a7363e8503b776bf7676d207066763fa60d9
Ilari Lehtonen <ilari.lehtonen@gmail.com>
2023-11-28 21:35:45 +0200

    1.5

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -3,7 +3,7 @@ const Header = (props) => {
 
   return (
     <>
-      <h1>{props.course}</h1>
+      <h1>{props.course.name}</h1>
     </>
   )
 
@@ -23,7 +23,7 @@ const Content = (props) => {
   console.log(props)
 
   return (
-    props.parts.map((part) =>
+    props.course.parts.map((part) =>
     <Part part={part}/>
     )
   )
@@ -32,7 +32,7 @@ const Content = (props) => {
 const Total = (props) => {
   console.log(props)
   let total = 0
-  props.parts.forEach(part => {
+  props.course.parts.forEach(part => {
     total += part.exercises
   });
 
@@ -44,29 +44,31 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   )
 }
-
+      
 export default App

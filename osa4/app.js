e518347517494d7bba227341a653e22fa49fb1a4
Ilari Lehtonen <ilari.lehtonen@gmail.com>
2024-01-07 20:24:42 +0200

    4.3-4.7

--- osa4/app.js
+++ osa4/app.js
@@ -1,5 +1,6 @@
 const config = require('./utils/config')
 const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -10,16 +11,19 @@ mongoose.set('strictQuery', false)
 
 logger.info('connecting to', config.MONGODB_URI)
 mongoose.connect(config.MONGODB_URI)
-    .then(() => {
-        logger.info('connected to MongoDB')
-    })
-    .catch((error) => {
-        logger.error('error connecting to MongoDB:', error.message)
-    })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.morgan(':method :url :status :res[content-length] - :response-time ms :postBody'))
 
 app.use('/api/blogs', blogsRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
---
+++ osa4/utils/middleware.js
@@ -0,0 +1,27 @@
+const morgan = require('morgan')
+
+morgan.token('postBody', (req) => {
+  if (req.method === 'POST') {
+    return (JSON.stringify(req.body))
+  }
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message })
+  }
+
+  next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+module.exports = {
+  errorHandler, unknownEndpoint, morgan
+}
